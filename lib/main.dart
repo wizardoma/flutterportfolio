fb7a1558633bdb02e152ccf4a48817985d237c9b
Ibekason Alexander <alibekason@gmail.com>
2021-04-30 14:28:33 +0100

    Add backgroundSection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,13 +17,17 @@ class _MyAppState extends State<MyApp> {
   String description =
       "Alex here, a highly motivated software developer with solid understanding of software development best practices and loves to build stuff, I'm also an open source hobbyist & tech enthusiast with a never easing desire to solve problems with software.👨‍";
 
+  String background = """I am a software developer who loves learning new things and solving problems. Using incredible tools and software, I love building softwares and services that make positive impacts to the community
+  I am an active member of various developers communities, where I focus on making impacts and sharing my experiences whilst learning.
+  Being a full-stack engineer, my goal is to always build applications that are scalable and efficient under the hood while providing engaging, adaptive, simple and efficient user experiences. On the other hand, community gives me a platform to share complementary ideas with my peers and accelerate the process of crafting better solutions without being distracted by the buzz, glitz and opportunities that seem to exist.
+  When I'm not in front of a computer screen, I'm probably reading a technical or self-help book, surfing the net for tech news and updates, hanging out or giving free therapy to a friend.""";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Ibekason Alexander",
       debugShowCheckedModeBanner: false,
       theme: isDarkMode
-          ? ThemeData.dark()
+          ? ThemeData.dark().copyWith(primaryColor: Colors.green)
           : ThemeData.light().copyWith(primaryColor: Colors.green),
       home: SafeArea(
         child: Scaffold(
@@ -68,10 +74,13 @@ class _MyAppState extends State<MyApp> {
             },
             body: Container(
               padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: ListView(
+                shrinkWrap: true,
+
                 children: [
                   introductionSection(),
+                  backgroundSection(),
+
                 ],
               ),
             ),
@@ -134,19 +143,40 @@ class _MyAppState extends State<MyApp> {
     var container = Container(
       child: Text(
         description,
-        style: TextStyle(
-            height: 1.5,
-            color: !isDarkMode ? Colors.black54 : Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.w400),
+        style: textStyle(
+          isDarkMode: isDarkMode,
+          fontSize: 22,
+          height: 1.5
+        )
       ),
     );
 
     return SectionWrapper(
-      title: "Hello!",
+      title: "Introduction",
       content: container,
       isDarkMode: isDarkMode,
     );
+  }
+
+  Widget backgroundSection() {
+    var container = Container(
+      child: Text(background, style: textStyle(
+        height: 1.3,
+        isDarkMode: isDarkMode,
+        fontSize: 18
+
+      ),),
+    );
+    return SectionWrapper(title: "Background", content: container,);
+  }
+
+
+  TextStyle textStyle({bool isDarkMode, double fontSize, double height}){
+    return TextStyle(
+        height: height,
+        color: !isDarkMode ? Colors.black54 : Colors.white,
+        fontSize: fontSize,
+        fontWeight: FontWeight.w400);
   }
 }
 
@@ -160,7 +190,6 @@ class SectionWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -168,16 +197,18 @@ class SectionWrapper extends StatelessWidget {
           child: Text(
             title,
             style: TextStyle(
-                color: !isDarkMode ? Colors.black54 : Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.w400),
+                color: Theme.of(context).primaryColor,
+                fontSize: 28,
+                fontWeight: FontWeight.bold),
           ),
         ),
         SizedBox(
           height: 10,
         ),
         Container(padding: EdgeInsets.only(left: 15), child: content),
+      SizedBox(height: 30,),
       ],
     );
   }
+
 }
